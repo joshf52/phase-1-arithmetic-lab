d744ad2d6b55ef03f0292834063c2c2c344015c9
Josh Fuller <joshf5252@gmail.com>
2021-12-12 22:46:00 -0500

    commit tree 079027dfb80cd0357dc4d89a4b7a739a06c8ce21

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2= 2
+let multiply = num1*num2
+
+const random = Math.ceil(Math.random()* 10);
+
+let num3 = 14
+let num4 = 5
+const mod = num3 % num4
+
+let max= Math.max(1,2,5,20);
 
 // Write your code here
